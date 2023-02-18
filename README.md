910e17192bfe50479c6d456f01f8a48f45c22935
devalvez <wesley.alves.jhenp@gmail.com>
2023-02-18 16:50:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Institute Layout
+
+Template construído como exemple de estudo.
+
+### Tecnologia
+
+- HTML5
+- CSS 3
+- JavaScript
+
+Visualizar no navegador: 
+
+![preview]('/../preview.jpg')
---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+let navigator = document.querySelector('#navigator');
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 100) {
+    navigator.classList.add('scrolled');
+    document.querySelector('#logo').setAttribute('src' ,'./assets/images/logo-light.png');
+  } else {
+    navigator.classList.remove('scrolled');
+    document.querySelector('#logo').setAttribute('src' ,'./assets/images/logo.png');
+  }
+});
+
+
+
+let inputName = document.querySelector('#name');
+let inputEmail = document.querySelector('#email');
+let inputMessage = document.querySelector('#message');
+
+document.querySelector('#action').addEventListener('click', () => {
+  let name = inputName.value;
+  let email = inputEmail.value;
+  let message = inputMessage.value;
+
+  if(name === '') {
+    document.querySelector('#err-name').innerHTML = "This field is required";
+  } else {
+    document.querySelector('#err-name').innerHTML = "";
+  }
+   
+  if(email === '') {
+    document.querySelector('#err-email').innerHTML = "This field is required";
+  } else {
+    document.querySelector('#err-email').innerHTML = "";
+  }
+  if(message === '') {
+    document.querySelector('#err-message').innerHTML = "This field is required";
+  } else {
+    document.querySelector('#err-message').innerHTML = "";
+  }
+
+});
+
